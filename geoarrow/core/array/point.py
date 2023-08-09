c4b88a6c21d2ab7c26b093f0bc3bb5c1d25bf073
Kyle Barron <kylebarron2@gmail.com>
2023-08-09 12:19:45 -0400

    Initial PointArray class

---
+++ geoarrow/core/array/point.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import pyarrow as pa
+
+if TYPE_CHECKING:
+    import geoarrow.geo
+    import geoarrow.geos
+    import geoarrow.proj
+
+
+@dataclass
+class PointArray:
+
+    arr: pa.Array
+
+    def __post_init__(self):
+        validate_point_array(self.arr)
+
+    @property
+    def geo(self) -> geoarrow.geo.PointArray:
+        """Access GeoRust algorithms on this PointArray"""
+        import geoarrow.geo
+
+        return geoarrow.geo.PointArray(self)
+
+    @property
+    def geos(self) -> geoarrow.geos.PointArray:
+        """Access GEOS algorithms on this PointArray"""
+        import geoarrow.geos
+
+        return geoarrow.geos.PointArray(self)
+
+    @property
+    def proj(self) -> geoarrow.proj.PointArray:
+        """Access Proj algorithms on this PointArray"""
+        import geoarrow.proj
+
+        return geoarrow.proj.PointArray(self)
+
+
+def validate_point_array(arr: pa.Array):
+    """Validate that this pyarrow Array is a valid GeoArrow Point array
+    """
+    pass
