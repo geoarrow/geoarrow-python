13319ebb359cd3691382ea0ac82f6320f9c35417
Dewey Dunnington <dewey@voltrondata.com>
2023-09-25 13:45:18 -0300

    fix name + dup test

--- geoarrow-pandas/tests/test_geoarrow_pandas_geopandas.py
+++ geoarrow-pandas/tests/test_geoarrow_pandas_geopandas.py
@@ -4,10 +4,10 @@ import pandas as pd
 import geoarrow.pandas as gapd
 import geoarrow.pyarrow as ga
 
-def test_accessor_to_geopandas():
+def test_scalar_to_shapely():
     series = pd.Series(["POINT (0 1)", "POINT (1 2)"])
     extension_series = series.geoarrow.as_geoarrow()
-    assert extension_series[0].as_shapely().wkt == "POINT (0 1)"
+    assert extension_series[0].to_shapely().wkt == "POINT (0 1)"
 
 def test_accessor_to_geopandas():
     series = pd.Series(["POINT (0 1)", "POINT (1 2)"])
