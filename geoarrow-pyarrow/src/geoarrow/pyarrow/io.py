546475bf6b7745b61932cce396f4cea355ecf0df
Dewey Dunnington <dewey@fishandwhistle.net>
2023-10-01 14:33:49 -0300

    add test

--- geoarrow-pyarrow/src/geoarrow/pyarrow/io.py
+++ geoarrow-pyarrow/src/geoarrow/pyarrow/io.py
@@ -6,18 +6,22 @@ def read_pyogrio_table(*args, **kwargs):
     import pyproj
 
     meta, table = read_arrow(*args, **kwargs)
-    # Maybe not always true? meta["geometry_name"] is occasionally empty ""
+
+    # Maybe not always true? meta["geometry_name"] is occasionally `""`
     geometry_name = meta["geometry_name"] if meta["geometry_name"] else "wkb_geometry"
 
-    # Create the geoarrow-enabled geometry column
+    # Create the geoarrow-enabled geometry column with a crs attribute
     prj = pyproj.CRS(meta["crs"])
-    geometry_type = _ga.wkb().with_crs(prj.to_json())
-    geometry = geometry_type.wrap_array(table.column(geometry_name))
+    geoarrow_type = _ga.wkb().with_crs(prj.to_json())
+    geometry = geoarrow_type.wrap_array(table.column(geometry_name))
 
-    # Move geometry column to the end and name it 'geometry'
+    # Apply CRS to geometry column. This doesn't scale to multiple geometry
+    # columns, but it's unclear if other columns would share the same CRS.
     for i, nm in enumerate(table.column_names):
         if nm == geometry_name:
-            table = table.remove_column(i)
+            geometry = table.column(i)
+            geometry = geoarrow_type.wrap_array(geometry)
+            table = table.set_column(i, nm, geometry)
             break
 
-    return table.append_column("geometry", geometry)
+    return table
