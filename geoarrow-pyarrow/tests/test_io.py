75bde27c6fcc6d663fbb37d31639872b414b7085
Dewey Dunnington <dewey@fishandwhistle.net>
2023-10-01 15:21:09 -0300

    fix test

--- geoarrow-pyarrow/tests/test_io.py
+++ geoarrow-pyarrow/tests/test_io.py
@@ -18,6 +18,26 @@ def test_readpyogrio_table():
         crs_json = df.geometry.crs.to_json()
         pyogrio.write_dataframe(df, temp_gpkg)
 
+        # With auto:
         table = io.read_pyogrio_table(temp_gpkg)
+        assert table.column("geom").type == ga.point().with_crs(crs_json)
+        assert ga.format_wkt(table.column("geom")).to_pylist() == ["POINT (0 1)"]
+
+        # Explicit coord type:
+        table = io.read_pyogrio_table(
+            temp_gpkg, geoarrow_coord_type=ga.CoordType.INTERLEAVED
+        )
+        assert table.column("geom").type == ga.point().with_coord_type(
+            ga.CoordType.INTERLEAVED
+        ).with_crs(crs_json)
+        assert ga.format_wkt(table.column("geom")).to_pylist() == ["POINT (0 1)"]
+
+        # Explicit promote multi:
+        table = io.read_pyogrio_table(temp_gpkg, geoarrow_promote_multi=True)
+        assert table.column("geom").type == ga.multipoint().with_crs(crs_json)
+        assert ga.format_wkt(table.column("geom")).to_pylist() == ["MULTIPOINT (0 1)"]
+
+        # Explicit type:
+        table = io.read_pyogrio_table(temp_gpkg, geoarrow_type=ga.wkb())
         assert table.column("geom").type == ga.wkb().with_crs(crs_json)
         assert ga.format_wkt(table.column("geom")).to_pylist() == ["POINT (0 1)"]
