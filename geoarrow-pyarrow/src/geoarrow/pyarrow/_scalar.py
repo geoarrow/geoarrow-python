148be64d693a43bb32ed4c5e7b8208b310dcc099
Dewey Dunnington <dewey@dunnington.ca>
2023-10-04 14:30:41 -0300

    Implement basic scalar methods (#28)

--- geoarrow-pyarrow/src/geoarrow/pyarrow/_scalar.py
+++ geoarrow-pyarrow/src/geoarrow/pyarrow/_scalar.py
@@ -1,9 +1,46 @@
 import pyarrow as pa
 
 from geoarrow.pyarrow._type import GeometryExtensionType
+from geoarrow.pyarrow._kernel import Kernel
 
 
 class GeometryExtensionScalar(pa.ExtensionScalar):
+    def __repr__(self):
+        # Before pyarrow 13.0.0 this will fail because constructing
+        # arrays from scalars
+        pa_version = [int(component) for component in pa.__version__.split(".")]
+        if pa_version[0] < 13:
+            return super().__repr__()
+
+        max_width = 70
+
+        try:
+            kernel = Kernel.format_wkt(self.type, max_element_size_bytes=max_width)
+            array_formatted = kernel.push(self._array1())
+            string_formatted = array_formatted[0].as_py()
+        except:
+            string_formatted = "<value failed to parse>"
+
+        if len(string_formatted) >= max_width:
+            string_formatted = string_formatted[: (max_width - 3)] + "..."
+
+        return f"{type(self).__name__}\n<{string_formatted}>"
+
+    def _array1(self):
+        return self.type.wrap_array(pa.array([self.value]))
+
+    @property
+    def wkt(self):
+        kernel = Kernel.as_wkt(self.type)
+        array_wkt = kernel.push(self._array1())
+        return array_wkt.storage[0].as_py()
+
+    @property
+    def wkb(self):
+        kernel = Kernel.as_wkb(self.type)
+        array_wkb = kernel.push(self._array1())
+        return array_wkb.storage[0].as_py()
+
     def to_shapely(self):
         """
         Convert an array item to a shapely geometry
@@ -13,21 +50,26 @@ class GeometryExtensionScalar(pa.ExtensionScalar):
         >>> array[0].to_shapely()
         <POINT (30 10)>
         """
-        raise NotImplementedError()
+        from shapely import from_wkb
+
+        return from_wkb(self.wkb)
 
 
-class WktScalar(pa.ExtensionScalar):
+class WktScalar(GeometryExtensionScalar):
+    @property
+    def wkt(self):
+        return self.value.as_py()
+
     def to_shapely(self):
         from shapely import from_wkt
 
         return from_wkt(self.value.as_py())
 
 
-class WkbScalar(pa.ExtensionScalar):
-    def to_shapely(self):
-        from shapely import from_wkb
-
-        return from_wkb(self.value.as_py())
+class WkbScalar(GeometryExtensionScalar):
+    @property
+    def wkb(self):
+        return self.value.as_py()
 
 
 class PointScalar(GeometryExtensionScalar):
