e38e787cb1af1d454a5a12ec1527fb977bdfdef1
Dewey Dunnington <dewey@fishandwhistle.net>
2023-11-10 15:44:04 -0400

    better geometry name when the name is not known

--- geoarrow-pyarrow/tests/test_io.py
+++ geoarrow-pyarrow/tests/test_io.py
@@ -8,11 +8,12 @@ import geoarrow.pyarrow as ga
 from geoarrow.pyarrow import io
 
 
-def test_readpyogrio_table():
+def test_readpyogrio_table_gpkg():
     pyogrio = pytest.importorskip("pyogrio")
     geopandas = pytest.importorskip("geopandas")
 
     with tempfile.TemporaryDirectory() as tmpdir:
+        # Check gpkg (which has internal geometry column name)
         temp_gpkg = os.path.join(tmpdir, "test.gpkg")
         df = geopandas.GeoDataFrame(
             geometry=geopandas.GeoSeries.from_wkt(["POINT (0 1)"], crs="OGC:CRS84")
@@ -23,6 +24,14 @@ def test_readpyogrio_table():
         table = io.read_pyogrio_table(temp_gpkg)
         assert table.column("geom").type == ga.wkb().with_crs(crs_json)
         assert ga.format_wkt(table.column("geom")).to_pylist() == ["POINT (0 1)"]
+
+        # Check fgb (which does not have an internal geometry column name)
+        temp_fgb = os.path.join(tmpdir, "test.fgb")
+        pyogrio.write_dataframe(df, temp_fgb)
+
+        table = io.read_pyogrio_table(temp_fgb)
+        assert table.column("geometry").type == ga.wkb().with_crs(crs_json)
+        assert ga.format_wkt(table.column("geometry")).to_pylist() == ["POINT (0 1)"]
 
 
 def test_write_geoparquet_table():
