f6e31ea781353e32d6884c6dac844e3697676e16
Dewey Dunnington <dewey@fishandwhistle.net>
2023-11-16 22:41:21 -0400

    ruff

--- geoarrow-pyarrow/tests/test_geopandas.py
+++ geoarrow-pyarrow/tests/test_geopandas.py
@@ -4,6 +4,7 @@ import geoarrow.pyarrow as ga
 
 geopandas = pytest.importorskip("geopandas")
 
+
 def test_from_geopandas():
     geoseries = geopandas.GeoSeries.from_wkt(["POINT (30 10)"]).set_crs("OGC:CRS84")
     array = ga.array(geoseries)
@@ -11,6 +12,7 @@ def test_from_geopandas():
     assert array.type.crs_type == ga.CrsType.PROJJSON
     assert "CRS84" in array.type.crs
     assert ga.format_wkt(array)[0].as_py() == "POINT (30 10)"
+
 
 def test_scalar_to_shapely():
     array = ga.array(["POINT (30 10)"])
