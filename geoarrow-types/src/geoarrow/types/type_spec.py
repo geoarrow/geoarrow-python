53759e6acccfc1d3a75fe52176d478eb7eef03e5
Dewey Dunnington <dewey@fishandwhistle.net>
2024-05-19 01:17:00 -0300

    simplify spec

--- geoarrow-types/src/geoarrow/types/type_spec.py
+++ geoarrow-types/src/geoarrow/types/type_spec.py
@@ -8,7 +8,8 @@ from geoarrow.types.constants import (
     EdgeType,
     CoordType,
 )
-from geoarrow.types.crs import Crs, UNSPECIFIED, create as crs_create
+from geoarrow.types.crs import Crs
+from geoarrow.types import crs
 
 
 class TypeSpec(NamedTuple):
@@ -17,7 +18,14 @@ class TypeSpec(NamedTuple):
     dimensions: Dimensions = Dimensions.UNSPECIFIED
     coord_type: CoordType = CoordType.UNSPECIFIED
     edge_type: EdgeType = EdgeType.UNSPECIFIED
-    crs: Optional[Crs] = UNSPECIFIED
+    crs: Optional[Crs] = crs.UNSPECIFIED
+
+    def any_unspecified(self) -> bool:
+        for cls, arg in zip(_SPEC_TYPES):
+            if arg == cls.UNSPECIFIED:
+                return True
+
+        return False
 
 
 def type_spec(
@@ -27,13 +35,11 @@ def type_spec(
     dimensions=None,
     coord_type=None,
     edge_type=None,
-    crs=UNSPECIFIED,
+    crs=crs.UNSPECIFIED,
 ):
-    return TypeSpec(
-        Encoding.create(encoding),
-        GeometryType.create(geometry_type),
-        Dimensions.create(dimensions),
-        CoordType.create(coord_type),
-        EdgeType.create(edge_type),
-        crs_create(crs),
-    )
+    args = (encoding, geometry_type, dimensions, coord_type, edge_type, crs)
+    sanitized_args = (cls.create(arg) for cls, arg in zip(_SPEC_TYPES, args))
+    return TypeSpec(*sanitized_args)
+
+
+_SPEC_TYPES = [Encoding, GeometryType, Dimensions, CoordType, EdgeType, crs]
