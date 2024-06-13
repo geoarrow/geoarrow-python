95762bd6973b6976529d902de68c142fd4a2f095
Dewey Dunnington <dewey@voltrondata.com>
2024-06-13 10:42:47 -0300

    test some accessors

--- geoarrow-types/tests/test_type_pyarrow.py
+++ geoarrow-types/tests/test_type_pyarrow.py
@@ -1,4 +1,5 @@
 import pyarrow as pa
+import pytest
 
 import geoarrow.types as gt
 from geoarrow.types import type_pyarrow
@@ -69,3 +70,66 @@ def test_geometry_types():
             False,
         )
     )
+
+    with pytest.raises(ValueError, match="Can't compute extension name"):
+        gt.type_spec(gt.GeometryType.GEOMETRYCOLLECTION).to_pyarrow()
+
+
+def test_interleaved_dimensions():
+    point_xy = gt.point(dimensions="xy", coord_type="interleaved").to_pyarrow()
+    assert point_xy.coord_type == gt.CoordType.INTERLEAVED
+    assert point_xy.dimensions == gt.Dimensions.XY
+    assert point_xy.storage_type.field(0).name == "xy"
+
+    point_xyz = gt.point(dimensions="xyz", coord_type="interleaved").to_pyarrow()
+    assert point_xyz.coord_type == gt.CoordType.INTERLEAVED
+    assert point_xyz.dimensions == gt.Dimensions.XYZ
+    assert point_xyz.storage_type.field(0).name == "xyz"
+
+    point_xym = gt.point(dimensions="xym", coord_type="interleaved").to_pyarrow()
+    assert point_xym.coord_type == gt.CoordType.INTERLEAVED
+    assert point_xym.dimensions == gt.Dimensions.XYM
+    assert point_xym.storage_type.field(0).name == "xym"
+
+    point_xyzm = gt.point(dimensions="xyzm", coord_type="interleaved").to_pyarrow()
+    assert point_xyzm.coord_type == gt.CoordType.INTERLEAVED
+    assert point_xyzm.dimensions == gt.Dimensions.XYZM
+    assert point_xyzm.storage_type.field(0).name == "xyzm"
+
+
+def test_separated_dimensions():
+    point_xy = gt.point(dimensions="xy", coord_type="separated").to_pyarrow()
+    assert point_xy.coord_type == gt.CoordType.SEPARATED
+    assert point_xy.dimensions == gt.Dimensions.XY
+    storage_names = [
+        point_xy.storage_type.field(i).name
+        for i in range(point_xy.storage_type.num_fields)
+    ]
+    assert storage_names == ["x", "y"]
+
+    point_xyz = gt.point(dimensions="xyz", coord_type="separated").to_pyarrow()
+    assert point_xy.coord_type == gt.CoordType.SEPARATED
+    assert point_xyz.dimensions == gt.Dimensions.XYZ
+    storage_names = [
+        point_xyz.storage_type.field(i).name
+        for i in range(point_xyz.storage_type.num_fields)
+    ]
+    assert storage_names == ["x", "y", "z"]
+
+    point_xym = gt.point(dimensions="xym", coord_type="separated").to_pyarrow()
+    assert point_xy.coord_type == gt.CoordType.SEPARATED
+    assert point_xym.dimensions == gt.Dimensions.XYM
+    storage_names = [
+        point_xym.storage_type.field(i).name
+        for i in range(point_xym.storage_type.num_fields)
+    ]
+    assert storage_names == ["x", "y", "m"]
+
+    point_xyzm = gt.point(dimensions="xyzm", coord_type="separated").to_pyarrow()
+    assert point_xy.coord_type == gt.CoordType.SEPARATED
+    assert point_xyzm.dimensions == gt.Dimensions.XYZM
+    storage_names = [
+        point_xyzm.storage_type.field(i).name
+        for i in range(point_xyzm.storage_type.num_fields)
+    ]
+    assert storage_names == ["x", "y", "z", "m"]
